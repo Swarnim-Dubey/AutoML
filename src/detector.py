9c66b7227ac8666b4e0ff8043db36c28e4ba1dcf
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-07-07 23:28:06 +0530

    feat: added detect and drop col

--- src/detector.py
+++ src/detector.py
@@ -20,3 +20,14 @@ def detect_problem_type(y):
 def profile_columns(X):
     numerical_col, categorical_col = get_columns_type(X)
     return numerical_col, categorical_col
+
+def detect_id_columns(df, threshold=0.95):
+    id_cols = []
+    for col in df.columns:
+        ratio = df[col].nunique() / len(df)
+        if ratio > threshold:
+            id_cols.append(col)
+    return id_cols
+
+def drop_id_columns(df, id_cols):
+    return df.drop(columns=id_cols)
